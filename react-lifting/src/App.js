0969eb70d96201b9a841a913a5da6acf21f8ea27
Alok Tiwari <alokt1994@gmail.com>
2024-08-15 00:37:34 +0530

    change in app.js

--- react-lifting/src/App.js
+++ react-lifting/src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
-// import Counter from "./lifting-state/Counter";
-import ParentComponent from "./lifting-state/ParentComponent";
+import Counter from "./lifting-state/Counter";
+// import ParentComponent from "./lifting-state/ParentComponent";
 function App() {
   return (
     <div>
-      {/* <Counter></Counter> */}
-      <ParentComponent />
+      <Counter></Counter>
+      {/* <ParentComponent /> */}
     </div>
   );
 }
