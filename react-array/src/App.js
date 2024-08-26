71aa86826bd65d8dfe81b080b36e4bb9876a72e7
alokt1994 <alokt1994@gmail.com>
2024-08-26 19:58:33 +0530

    add array rendering program

--- react-array/src/App.js
+++ react-array/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Renderarray from './Component/Renderarray';
+// import Arraymap from './Arraymap';
+// import Mappingarrayofarray from './Mappingarrayofarray';
+// import Mappingarrayobject from './Mappingarrayobject';
 
-function App() {
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Renderarray />
+      {/* <Arraymap />
+      <Mappingarrayofarray />
+      <Mappingarrayobject /> */}
     </div>
   );
 }
-
-export default App;
---
+++ react-array/src/Component/Renderarray.js
@@ -0,0 +1,15 @@
+// App.js
+import React from 'react';
+
+const Renderarray = () => {
+  return (
+    <div className="container">
+      <div>
+        <h1>Numbers List</h1>
+        {[1, 2, 3, 4, 5]}
+      </div>
+    </div>
+  );
+};
+
+export default Renderarray;
